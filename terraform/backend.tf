0063591948107675f3b67ad6ad12e02832c79c23
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-26 11:58:37 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-two-tier-vamsee"
-    key            = "terraform/statefile_lambda"
-    region         = "ap-south-1"
-  }
-}
--- terraform/modules/ecr/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "app_repository" {
-  name = var.repository_name
-}
