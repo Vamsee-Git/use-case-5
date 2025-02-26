211f289aa282e5f145bb39ece45db958b1147587
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-26 12:11:55 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "this" {
+resource "aws_lambda_function" "lambda_container" {
   function_name = var.lambda_function_name
   role          = var.iam_role_arn
   package_type  = "Image"
@@ -17,59 +17,59 @@ resource "aws_lambda_function" "this" {
 }
 
 # API Gateway to trigger the Lambda function
-resource "aws_api_gateway_rest_api" "this" {
+resource "aws_api_gateway_rest_api" "lambda_container" {
   name        = "${var.lambda_function_name}-api"
   description = "API Gateway to trigger ${var.lambda_function_name}"
 }
 
-resource "aws_api_gateway_resource" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+resource "aws_api_gateway_resource" "lambda_container" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_container.id
+  parent_id   = aws_api_gateway_rest_api.lambda_container.root_resource_id
   path_part   = "invoke"
 }
 
-resource "aws_api_gateway_method" "this" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.this.id
+resource "aws_api_gateway_method" "lambda_container" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_container.id
+  resource_id   = aws_api_gateway_resource.lambda_container.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "this" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.this.id
-  http_method             = aws_api_gateway_method.this.http_method
+resource "aws_api_gateway_integration" "lambda_container" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda_container.id
+  resource_id             = aws_api_gateway_resource.lambda_container.id
+  http_method             = aws_api_gateway_method.lambda_container.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
 }
 
 # Lambda permissions for API Gateway to invoke the function
-resource "aws_lambda_permission" "this" {
+resource "aws_lambda_permission" "lambda_container" {
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.function_name
+  function_name = aws_lambda_function.lambda_container.function_name
   principal     = "apigateway.amazonaws.com"
 }
 
 # Deploy API Gateway to a stage
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
+resource "aws_api_gateway_deployment" "lambda_container" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_container.id
   stage_name  = var.api_stage
 
   depends_on = [
-    aws_api_gateway_integration.this,
-    aws_api_gateway_method.this
+    aws_api_gateway_integration.lambda_container,
+    aws_api_gateway_method.lambda_container
   ]
 }
 
 output "lambda_invoke_arn" {
-  value = aws_lambda_function.this.invoke_arn
+  value = aws_lambda_function.lambda_container.invoke_arn
 }
 
 output "lambda_function_arn" {
-  value = aws_lambda_function.this.arn
+  value = aws_lambda_function.lambda_container.arn
 }
 
 output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/${var.api_stage}/invoke"
+  value = "https://${aws_api_gateway_rest_api.lambda_container.id}.execute-api.${var.region}.amazonaws.com/${var.api_stage}/invoke"
 }
